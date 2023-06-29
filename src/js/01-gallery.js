38976387b2243740736f56b23c7a4186d531a1c6
Sergii-Mykolayovych <123812218+Sergii-Mykolayovych@users.noreply.github.com>
2023-06-29 11:48:29 +0300

    Modul 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,11 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import '../css/01-gallery.css';
 import '../css/common.css';
 
-// console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 
